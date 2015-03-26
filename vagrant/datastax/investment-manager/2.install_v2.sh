bb5af818bd87879de704a99f870d40f84f458706
Joaquin Casares <joaquin@datastax.com>
2015-03-26 15:08:12 -0500

    Add first building steps

--- vagrant/datastax/investment-manager/2.install_v2.sh
+++ vagrant/datastax/investment-manager/2.install_v2.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 set -x # echo on
+
+cd /cornerstone/web/datastax/investment-manager/StockData
+mvn package
+mvn install:install-file -DgroupId=StockData -DartifactId=StockData \
+    -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar \
+    -Dfile=/cornerstone/web/datastax/investment-manager/StockData/target/StockData-0.0.1-SNAPSHOT.jar
+
+cd /cornerstone/web/datastax/investment-manager/Portfolio
+mvn package
---
+++ vagrant/general/apt/install/maven2_v1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -x # echo on
+
+FILENAME=maven2_v1.sh
+
+PACKAGES='maven2'
+CACHE=/cache/apt/$FILENAME
+
+if [ ! -d ${CACHE} ]; then
+    mkdir -p ${CACHE}
+    apt-get --print-uris --yes install $PACKAGES | grep ^\' | cut -d\' -f2 > ${CACHE}.list
+    wget -c -i ${CACHE}.list -P ${CACHE}
+fi
+sudo dpkg -i ${CACHE}/*
