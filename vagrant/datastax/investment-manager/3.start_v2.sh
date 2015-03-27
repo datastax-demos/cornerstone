6506c82a5ff3f60ccda5e6db629cd9ea99750194
Joaquin Casares <joaquin@datastax.com>
2015-03-27 13:16:52 -0500

    Name schemas by keyspace

--- vagrant/datastax/investment-manager/3.start_v2.sh
+++ vagrant/datastax/investment-manager/3.start_v2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x # echo on
 
-cqlsh -f /cornerstone/cql/datastax/investment-manager/schema.cql
+cqlsh -f /cornerstone/cql/datastax/investment-manager/stocks.cql
 
 $CATALINA_HOME/bin/startup.sh
