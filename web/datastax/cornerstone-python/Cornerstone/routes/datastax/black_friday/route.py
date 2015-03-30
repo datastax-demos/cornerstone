8a314d4388ed06e5a5c5030622f5faf9f31b0261
Joaquin Casares <joaquin@datastax.com>
2015-03-30 18:29:08 -0500

    resorting links

--- web/datastax/cornerstone-python/Cornerstone/routes/datastax/black_friday/route.py
+++ web/datastax/cornerstone-python/Cornerstone/routes/datastax/black_friday/route.py
@@ -18,18 +18,30 @@ def index():
             <a href="/api/paging/system/schema_keyspaces">API: system.schema_keyspaces</a>
         </li>
         <li>
+            <a href="/gcharts/table/?gcharts_columns=compacted_at,id,keyspace_name,columnfamily_name,rows_merged,bytes_in,bytes_out&result_size=100">
+                Google Charts: Tabular Example
+            </a>
+        </li>
+    </ul>
+    <ul>
+        <li>
             <a href="/api/paging/retail/products?result_size=5&paging_keys=product_id&paging_values=B000ET7AZK">
                 API: Sample Query with Custom Values
             </a>
         </li>
         <li>
-            <a href="/gcharts/table/?gcharts_columns=compacted_at,id,keyspace_name,columnfamily_name,rows_merged,bytes_in,bytes_out&result_size=100">
-                Google Charts: Tabular Example
+            <a href="/gcharts/barchart/?url=/api/paging/retail/products&result_size=100&gcharts_columns=product_id,price&gcharts_datatable_order_by=price">
+                Google Charts: Bar Chart Example
             </a>
         </li>
         <li>
-            <a href="/gcharts/barchart/?url=/api/paging/retail/products&result_size=100&gcharts_columns=product_id,price&gcharts_datatable_order_by=price">
-                Google Charts: Bar Chart Example
+            <a href="/gcharts/table/?url=/api/paging/retail/employees&result_size=100">
+                Google Charts: Employees Table
+            </a>
+        </li>
+        <li>
+            <a href="/gcharts/table/?url=/api/paging/retail/stores&result_size=100">
+                Google Charts: Stores Table
             </a>
         </li>
     </ul>
