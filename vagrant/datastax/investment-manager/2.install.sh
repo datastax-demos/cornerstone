01222de5b593a570dabb32afab7a6a913a18ac88
Joaquin Casares <joaquin@datastax.com>
2015-04-10 10:33:34 -0500

    Wait to build investment manager until we have dse information

--- vagrant/datastax/investment-manager/2.install.sh
+++ vagrant/datastax/investment-manager/2.install.sh
@@ -4,15 +4,3 @@ set -x # echo on
 bash /cornerstone/vagrant/general/apt/install/jdk7_v2.sh
 bash /cornerstone/vagrant/general/apt/install/maven2_v1.sh
 bash /cornerstone/vagrant/general/file/install/tomcat_v2.sh
-
-cd /cornerstone/web/datastax/investment-manager/StockData
-mvn clean package
-mvn install:install-file -DgroupId=StockData -DartifactId=StockData \
-    -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar \
-    -Dfile=/cornerstone/web/datastax/investment-manager/StockData/target/StockData-0.0.1-SNAPSHOT.jar
-
-cd /cornerstone/web/datastax/investment-manager/Portfolio
-mvn clean package
-
-rm -rf /cache/installed/apache-tomcat-8.0.20/webapps/Portfolio
-cp /cornerstone/web/datastax/investment-manager/Portfolio/target/Portfolio-0.0.1-SNAPSHOT.war /cache/installed/apache-tomcat-8.0.20/webapps/Portfolio.war
