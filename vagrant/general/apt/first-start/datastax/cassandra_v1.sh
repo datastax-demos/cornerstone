e7e8558f1b26dae3644e36293809aa48b7233a22
Joaquin Casares <joaquin@datastax.com>
2015-04-10 11:30:39 -0500

    Update first-start scripts for multi-node setups

--- vagrant/general/apt/first-start/datastax/cassandra_v1.sh
+++ vagrant/general/apt/first-start/datastax/cassandra_v1.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
 set -x # echo on
 
-bash /cornerstone/vagrant/general/apt/configure/datastax-enterprise/address_v2.sh
+THIS_HOST=$(hostname -i | awk '{ print $2}')
+SEEDS=$THIS_HOST
+OPSCENTER_HOST=$THIS_HOST
+
+while [[ $# > 1 ]]
+do
+key="$1"
+case $key in
+    -s|--seeds)
+    SEEDS="$2"
+    shift
+    ;;
+    -h|--opscenter-host)
+    OPSCENTER_HOST="$2"
+    shift
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+shift
+done
+
+bash /cornerstone/vagrant/general/apt/configure/datastax-enterprise/address_v2.sh \
+    --listen-address $THIS_HOST --rpc-address $THIS_HOST
 bash /cornerstone/vagrant/general/apt/configure/datastax-enterprise/heap_v2.sh
-bash /cornerstone/vagrant/general/apt/configure/datastax-enterprise/seeds_v2.sh
-bash /cornerstone/vagrant/general/apt/configure/datastax-agent/stomp-interface_v2.sh
-bash /cornerstone/vagrant/general/apt/start/datastax-enterprise_v2.sh
+bash /cornerstone/vagrant/general/apt/configure/datastax-enterprise/seeds_v2.sh \
+    --seeds $SEEDS
+bash /cornerstone/vagrant/general/apt/configure/datastax-agent/stomp-interface_v2.sh \
+    --opscenter-host $OPSCENTER_HOST
+bash /cornerstone/vagrant/general/apt/start/datastax-enterprise_v2.sh \
+    --host $THIS_HOST
 bash /cornerstone/vagrant/general/apt/start/datastax-agent_v2.sh
--- vagrant/general/apt/first-start/datastax/opscenter_v1.sh
+++ vagrant/general/apt/first-start/datastax/opscenter_v1.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 set -x # echo on
 
-bash /cornerstone/vagrant/general/apt/configure/opscenter/seed-hosts_v2.sh
+SEEDS='127.0.0.1'
+
+while [[ $# > 1 ]]
+do
+key="$1"
+case $key in
+    -s|--seeds)
+    SEEDS="$2"
+    shift
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+shift
+done
+
+bash /cornerstone/vagrant/general/apt/configure/opscenter/seed-hosts_v2.sh \
+    --seeds $SEEDS
 bash /cornerstone/vagrant/general/apt/start/opscenter_v2.sh
